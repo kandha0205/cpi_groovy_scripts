fe9bf3aae141bf75a6b1bda133ec9cbd6ebffb15
munisxipi@gmail.com <munisxipi@gmail.com>
2021-09-01 18:05:54 +0200

    addding missing files

--- cpi-scripts/src/main/java/hello.groovy
+++ cpi-scripts/src/main/java/hello.groovy
@@ -1,3 +1,14 @@
 
-class hello {
+
+import com.sap.gateway.ip.core.customdev.util.Message
+import java.util.HashMap
+
+def Message processData(Message message) {
+    def body = message.getBody(java.lang.String)
+    def messageLog = messageLogFactory.getMessageLog(message)
+    if (messageLog != null) {
+        messageLog.addAttachmentAsString('Payload', body, 'text/plain')
+    }
+    return message
 }
+print "test"
